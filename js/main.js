8952d946869149ce712947a15e5c6ed9e6c46bd9
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2023-08-04 17:01:15 -0500

    add cached els and event listeners

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
   /*----- constants -----*/
+const PLAYERS = {
+  '0': '',
+  '1': 'Player One',
+  '-1': 'Player Two'
+}
 
+const FRUITS = {
+  'cherry': '..Memory-Card-Images/Cherry.jpg',
+  'grape': '..Memory-Card-Images/Grape.jpg',
+  'kiwi': '..Memory-Card-Images/Kiwi.jpg',
+  'orange': '..Memory-Card-Images/Orange.jpg',
+  'pineapple': '..Memory-Card-Images/Pineapple.jpg',
+  'watermelon': '..Memory-Card-Images/Watermelon.jpg'
+}
 
   /*----- state variables -----*/
-
+let turn, board, scores, winner
 
   /*----- cached elements  -----*/
-
+const messageEl = document.querySelector('h1')
+const playAgainBtn = document.querySelector('button')
+const boardEls = [...document.querySelectorAll('#board > div')]
+const p1Score = document.getElementById('p1-score')
+const p2Score = document.getElementById('p2-score')
 
   /*----- event listeners -----*/
-
+document.getElementById('board').addEventListener('click', handleClick)
+playAgainBtn.addEventListener('click', init)
 
   /*----- functions -----*/
 
+function handleClick() {
+  console.log(click)
+}
 
   let seconds = 3;
 function countdown() {
